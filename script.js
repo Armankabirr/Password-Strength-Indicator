71d0916ec3f61a5fb62b57dabbc76cefd7e04455
Armankabirr <armanutsho12@gmail.com>
2026-02-09 21:52:20 +0600

    Added colored indicator

--- script.js
+++ script.js
@@ -13,11 +13,20 @@ passwordInput.addEventListener("input", ()=>{
 
     if(passwordInput.value.length <= 4){
         indicator.innerText = "weak";
+        passwordInput.style.borderColor = "#ff4757";
+        message.style.color = "#ff4757";
+        icon.style.color = "#ff4757";
     }
     else if(passwordInput.value.length <= 8){
         indicator.innerText = "medium";
+        passwordInput.style.borderColor = "#ffa502";
+        message.style.color = "#ffa502";
+        icon.style.color = "#ffa502";
     }
     else{
         indicator.innerText = "strong";
+        passwordInput.style.borderColor = "#2ed573";
+        message.style.color = "#2ed573";
+        icon.style.color = "#2ed573";
     }
 })
